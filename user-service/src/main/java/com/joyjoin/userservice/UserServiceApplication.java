edb247bb0f7ddf27cd581e8ebd49446d9a1faa4b
JosipHarambasic <josip@Josips-MacBook-Pro.local>
2024-04-26 10:28:58 +0200

    remove unnecessary imports

--- user-service/src/main/java/com/joyjoin/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/joyjoin/userservice/UserServiceApplication.java
@@ -1,20 +1,14 @@
 package com.joyjoin.userservice;
 
-import com.joyjoin.userservice.model.User;
 import com.joyjoin.userservice.repository.UserRepository;
 import com.joyjoin.userservice.security.service.AuthService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.time.LocalDate;
-import java.util.Date;
 
 @SpringBootApplication
 @EnableFeignClients
