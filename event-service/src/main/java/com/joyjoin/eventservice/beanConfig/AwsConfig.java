e1a7aa858f9b98d1e76c843dab7819b25e5eac42
Karl Liu <karl.liu.1024@gmail.com>
2024-04-28 04:17:56 +0200

    event libs3

--- event-service/src/main/java/com/joyjoin/eventservice/beanConfig/AwsConfig.java
+++ event-service/src/main/java/com/joyjoin/eventservice/beanConfig/AwsConfig.java
@@ -1,5 +1,7 @@
 package com.joyjoin.eventservice.beanConfig;
 
+import com.joyjoin.libs3.S3Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -10,18 +12,6 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import java.net.URI;
 
 @Configuration
-public class AwsConfig {
-
-    final URI endpoint = URI.create("https://s3.localhost.localstack.cloud:4566");
-//    final URI endpoint = URI.create("http://localhost:4566");
-    final String ACCESS_KEY = "test";
-    final String SECRET_KEY = "test";
-    final StaticCredentialsProvider credentials = StaticCredentialsProvider.create(AwsBasicCredentials.create(ACCESS_KEY, SECRET_KEY));
-
-    final Region region = Region.US_EAST_1;
-
-    @Bean
-    public S3Presigner awsS3PreSigner() {
-        return S3Presigner.builder().endpointOverride(endpoint).credentialsProvider(credentials).region(region).build();
-    }
+@ConfigurationProperties("s3")
+public class AwsConfig extends S3Config {
 }
