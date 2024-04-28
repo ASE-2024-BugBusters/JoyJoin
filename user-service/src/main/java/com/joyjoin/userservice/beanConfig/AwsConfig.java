7306b3a9c2ae74bd5a5a3405dbe7ec362adc5e66
Karl Liu <karl.liu.1024@gmail.com>
2024-04-28 02:20:01 +0200

    load s3 configuration from configuration file

--- user-service/src/main/java/com/joyjoin/userservice/beanConfig/AwsConfig.java
+++ user-service/src/main/java/com/joyjoin/userservice/beanConfig/AwsConfig.java
@@ -1,5 +1,8 @@
 package com.joyjoin.userservice.beanConfig;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -11,17 +14,20 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import java.net.URI;
 
 @Configuration
+@ConfigurationProperties(prefix = "s3")
+@Getter
+@Setter
 public class AwsConfig {
-
-    final URI endpoint = URI.create("https://s3.localhost.localstack.cloud:4566");
-    final String ACCESS_KEY = "test";
-    final String SECRET_KEY = "test";
-    final StaticCredentialsProvider credentials = StaticCredentialsProvider.create(AwsBasicCredentials.create(ACCESS_KEY, SECRET_KEY));
-
-    final Region region = Region.US_EAST_1;
+    private String ACCESS_KEY;
+    private String SECRET_KEY;
+    private String ENDPOINT;
+    private String REGION;
 
     @Bean
     public S3Presigner awsS3PreSigner() {
+        final URI endpoint = URI.create(ENDPOINT);
+        final StaticCredentialsProvider credentials = StaticCredentialsProvider.create(AwsBasicCredentials.create(ACCESS_KEY, SECRET_KEY));
+        final Region region = Region.of(REGION);
         return S3Presigner.builder().endpointOverride(endpoint).credentialsProvider(credentials).region(region).build();
     }
 }
